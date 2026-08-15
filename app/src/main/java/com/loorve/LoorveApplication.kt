464f2876df336fdd99278b086dd7033d04b9df07
guswoths <hjson7585@inha.edu>
2026-08-15 19:38:33 +0900

    feat: add LoorveApplication with @HiltAndroidApp

--- app/src/main/java/com/loorve/LoorveApplication.kt
+++ app/src/main/java/com/loorve/LoorveApplication.kt
@@ -6,10 +6,8 @@ import dagger.hilt.android.HiltAndroidApp
 /**
  * LoorveApplication
  *
- * Hilt DI 프레임워크의 진입점.
- * @HiltAndroidApp 어노테이션이 KSP를 통해 Hilt 컴포넌트를 자동 생성한다.
- *
- * AndroidManifest.xml의 android:name=".LoorveApplication"과 반드시 일치해야 함.
+ * @HiltAndroidApp 어노테이션으로 Hilt DI 컨테이너 초기화.
+ * AndroidManifest.xml의 android:name=".LoorveApplication"과 반드시 일치해야 합니다.
  */
 @HiltAndroidApp
 class LoorveApplication : Application()
