0f1a1f8d30cc9d1e188127d93227f06040c7bb0e
guswoths <hjson7585@inha.edu>
2026-08-20 10:58:17 +0900

    fix: Add missing ProgressRepositoryImpl to resolve KSP build error

--- app/src/main/java/com/loorve/domain/repository/ProgressRepository.kt
+++ app/src/main/java/com/loorve/domain/repository/ProgressRepository.kt
@@ -1,12 +1,6 @@
 package com.loorve.domain.repository
 
-import com.loorve.domain.model.Progress
-import kotlinx.coroutines.flow.Flow
-
 interface ProgressRepository {
-    suspend fun saveProgress(uid: String, progress: Progress): Result<Unit>
-    suspend fun getProgressById(uid: String, progressId: String): Result<Progress>
-    suspend fun getProgressList(uid: String): Result<List<Progress>>
-    suspend fun deleteProgress(uid: String, progressId: String): Result<Unit>
-    fun observeProgressList(uid: String): Flow<List<Progress>>
+    // TODO: 진도 관련 도메인 메서드 정의
+    // suspend fun getProgress(userId: String): Progress
 }
