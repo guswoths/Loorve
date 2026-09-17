766907a293badd3092afc74a2d965f0c8e2b2eb5
guswoths <hjson7585@inha.edu>
2026-09-17 22:47:23 +0900

    build: bump versionCode to 10 and add BILLING permission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         applicationId = "com.loorve_2"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
+        versionCode = 10
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
