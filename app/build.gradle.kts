1951d00c2b58231b9ac7b0d5292ae87649120bef
guswoths <hjson7585@inha.edu>
2026-09-18 02:32:14 +0900

    build: bump versionCode by 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.loorve_2"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
-        versionName = "1.0.5"
+        versionCode = 22
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
